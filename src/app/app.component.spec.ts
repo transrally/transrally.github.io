764eca708ffb45953ac7280dd046e9077f8981d3
Usi Diamond <28960829+UsiDiamond@users.noreply.github.com>
2024-05-20 00:50:54 -0400

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,6 +21,6 @@ describe('AppComponent', () => {
   it(`should have as title 'TransRally Website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual(TransRally Website');
+    expect(app.title).toEqual('TransRally Website');
   });
 });
